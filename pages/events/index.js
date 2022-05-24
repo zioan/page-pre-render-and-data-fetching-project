e2dc48eb31a5e9c4ee81f40f8a34e1c6d06b7b2c
Ioan Zaharia <zaharia.ioan@gmail.com>
2022-05-24 12:32:41 +0200

    server side, static props, client side rendering

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,17 +1,15 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
-
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
+import { useRouter } from 'next/router';
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
-
+  const { events } = props;
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
-    
+
     router.push(fullPath);
   }
 
@@ -21,6 +19,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEventsPage;
